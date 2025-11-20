53b45f5f5c01848012606cc9932cc88b57ce78d6
i.afanasyev <i.afanasyev@samsung.com>
2025-11-20 08:35:46 +0000

    coalcesed version

--- cuda_kernels/sgemm.cu
+++ cuda_kernels/sgemm.cu
@@ -255,27 +255,48 @@ __global__ void sgemm_naive(const float *A,
                             float *C, 
                             int M, int N, int K,
                             float alpha, float beta) {
-  // compute position in C that this thread is responsible for
-  const uint x = blockIdx.x * blockDim.x + threadIdx.x;
-  const uint y = blockIdx.y * blockDim.y + threadIdx.y;
+    const uint x = blockIdx.x * blockDim.x + threadIdx.x;
+    const uint y = blockIdx.y * blockDim.y + threadIdx.y;
 
-  // `if` condition is necessary for when M or N aren't multiples of 32.
-  if (x < M && y < N) {
+    if (x >= M || y >= N)
+        return;
+    
     float tmp = 0.0;
+
     for (int i = 0; i < K; ++i) {
-      tmp += A[x * K + i] * B[i * N + y];
+        tmp += A[x * K + i] * B[i * N + y];
     }
-    // C = α*(A@B)+β*C
+
     C[x * N + y] = alpha * tmp + beta * C[x * N + y];
-  }
 }
 
+__global__ void sgemm_coalcesed(const float *A,
+                                const float *B, 
+                                float *C, 
+                                int M, int N, int K,
+                                float alpha, float beta) {
+    const uint x = blockIdx.x * blockDim.x + threadIdx.x;
+    const uint y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x >= M || y >= N)
+        return;
+    
+    float tmp = 0.0;
+
+    for (int i = 0; i < K; ++i) {
+        // we improved memory access patter here, 
+        tmp += A[y * K + i] * B[i * N + x];
+    }
+
+    C[y * N + x] = alpha * tmp + beta * C[y * N + x];
+}
 
 // --------------------------------------- Main -------------------------------------------
 
 int main(int argc, char** argv)
 {
     const bool verify = true;
+    const bool run_slow = false;
 
     int size_sq = 4096;
 
@@ -332,11 +353,17 @@ int main(int argc, char** argv)
         run_cublas_gemm(handle, M, N, K, dA, dB, dC, iters);
     }
 
-    // naive testing
+    if(run_slow)
     {
         dim3 grid_size(CEIL_DIV(M, 32), CEIL_DIV(N, 32), 1);
         dim3 block_size(32, 32, 1);
         run_custom_sgemm<sgemm_naive>(handle, dA, dB, dC, M, N, K, iters, block_size, grid_size, "naive global", verify);
+    }
+
+    {
+        dim3 grid_size(CEIL_DIV(M, 32), CEIL_DIV(N, 32), 1);
+        dim3 block_size(32, 32, 1);
+        run_custom_sgemm<sgemm_coalcesed>(handle, dA, dB, dC, M, N, K, iters, block_size, grid_size, "coalcesed global", verify);
     }
     
     CHECK_CUBLAS(cublasDestroy(handle));
